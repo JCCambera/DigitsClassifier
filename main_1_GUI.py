32a3662e0aeaa3396cd9757806d0429f3053603c
JCCambera <38401139+JCCambera@users.noreply.github.com>
2018-04-15 17:16:04 +0200

    Initial structure of the project

---
+++ main_1_GUI.py
@@ -0,0 +1,129 @@
+import tkinter as Tk
+from tkinter import ttk
+import PIL
+from PIL import Image, ImageDraw, ImageTk
+
+import numpy as np
+import matplotlib
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+color_white = (255, 255, 255)
+color_black = (0, 0, 0)
+color_green = (255, 0, 0)
+
+width_canvas = 400
+height_canvas = 400
+
+width_image_low_res = 20
+height_image_low_res = 20
+
+pen_width = 40
+
+width_application = 600 * 2
+height_application = 600 * 2
+
+file_name_blank = "blank_img.png"
+file_name = "number.png"
+file_name_resized = "number_resized.png"
+
+
+class MainWindow():
+    def __init__(self, parent):
+        # creation of the canvas
+        self.root = parent
+
+        self.canvas_1 = Tk.Canvas(self.root, width=width_canvas, height=height_canvas,relief='sunken')
+        self.canvas_1.grid(row=0, column=0)
+
+        # self.canvas_2 = Tk.Canvas(self.root, width=width_canvas, height=height_canvas)
+        self.canvas_2 = Tk.Canvas(self.root, width=width_canvas, height=height_canvas,relief='sunken')
+        self.canvas_2.grid(row=0, column=1)
+
+        self.canvas_3 = Tk.Canvas(self.root, width=width_canvas, height=height_canvas, relief='sunken')
+        self.canvas_3.grid(row=0, column=2)
+
+        image_blank = ImageTk.PhotoImage(file=file_name_blank)
+
+        self.img_label_canvas_2 = ttk.Label(self.canvas_2, image=image_blank)
+        self.img_label_canvas_2.img_1 = image_blank
+        self.img_label_canvas_2.config(compound = 'center')
+        self.img_label_canvas_2.grid(row=0, column=0)
+
+        self.img_label_canvas_3 = ttk.Label(self.canvas_3, image=image_blank, text='No classification',font = ('Courier',30,'bold'))
+        self.img_label_canvas_3.img_1 = image_blank
+        self.img_label_canvas_3.config(compound='center')
+        self.img_label_canvas_3.grid(row=0, column=0)
+
+        self.button_1 = Tk.Button(root, text="Save", command=self.on_btn1_save)
+        self.button_1.grid(row=1, column=0)
+
+        self.button_2 = Tk.Button(root, text="Preprocess", command=self.on_btn2_ld_preprocess_img)
+        self.button_2.grid(row=1, column=1)
+
+        self.button_3 = Tk.Button(root, text="Classify", command=self.on_btn3_classify)
+        self.button_3.grid(row=1, column=2)
+
+        # Drawing the number in the canvas
+        self.mouse_lb_pressed = False # The mouse left button is not initially pressed
+        self.drawingCoords = []
+        self.canvas_1.bind("<B1-Motion>", self.on_moving_mouse)
+        self.canvas_1.bind("<ButtonPress-1>", self.on_mouse_lb_pressed)
+        self.canvas_1.bind("<ButtonRelease-1>", self.on_mouse_lb_released)
+
+        # Saving the files in the
+
+        self.image_2_save = Image.new("RGB", (width_canvas, height_canvas), color_white)
+
+    def on_btn1_save(self):
+        print('saving')
+        image_2_save = Image.new("RGB", (width_canvas, height_canvas), color_white)
+        draw_obj = ImageDraw.Draw(image_2_save)
+
+        for point in self.drawingCoords:
+            print(point)
+            xy = [point[0] - pen_width / 2, point[1] - pen_width / 2, point[0] + pen_width / 2,
+                point[1] + pen_width / 2]
+            draw_obj.ellipse(xy, fill='black', outline=None)
+        # saving the regular image
+        image_2_save.save(file_name)
+
+        # saving the scaled image
+        image_2_save_resized = image_2_save.resize([width_image_low_res, height_image_low_res], PIL.Image.ANTIALIAS)
+        image_2_save_resized.save(file_name_resized)
+
+
+    def on_btn2_ld_preprocess_img(self):
+        print('preprocessing')
+
+        image_1 = Image.open(file_name_resized)
+        image_1_resized = image_1.resize((width_canvas, height_canvas))
+        photo_1 = ImageTk.PhotoImage(image_1_resized)
+
+        self.img_label_canvas_2.config(image=photo_1)
+        self.img_label_canvas_2.img_1 = photo_1
+
+    def on_moving_mouse(self,event):
+        # Ploting the draw into canvas 1
+        if self.mouse_lb_pressed:
+            self.canvas_1.create_oval(event.x - pen_width / 2, event.y - pen_width / 2, event.x + pen_width / 2,
+                event.y + pen_width / 2, fill="black")
+            self.drawingCoords.append((event.x, event.y))
+
+    def on_mouse_lb_pressed(self,event):
+        self.mouse_lb_pressed = True
+
+    def on_mouse_lb_released(self,event):
+        self.mouse_lb_pressed = False
+
+    def on_btn3_classify(self):
+
+        self.img_label_canvas_3.config(image=self.img_label_canvas_3.img_1)
+        self.img_label_canvas_3.config(text='Classified')
+                
+
+root = Tk.Tk()
+root.configure(background= 'gray')
+MainWindow(root)
+root.mainloop()
---
+++ main_2_Classifier.py
@@ -0,0 +1,103 @@
+# TODO To lower the reesolution of the data set and save it into a file (Create a class to do this), it also should normalize, and convert it to  no array to be accepted by Sklearn
+# TODO To configure the class in this file to accept as input the data set generated from the previous class
+
+from mnist import MNIST
+from math import sqrt
+import random
+from sklearn import datasets, svm, metrics
+import numpy as np
+
+class DigitsClassifier:
+    def __init__(self):
+
+        self.training_set_length = 600
+        self.test_set_length = 200
+
+
+        # self.load_DigitsData_ScikitLearn()
+        self.load_DigitsTrainningData()
+        self.classifierDef_SVM(self.training_set_length)
+
+    def load_DigitsData_ScikitLearn(self):
+        digits = datasets.load_digits()
+
+        self.images_training = digits.images[:self.training_set_length]
+        self.labels_training = digits.target[:self.training_set_length]
+        self.images_testing = digits.images[self.training_set_length+1:self.training_set_length+self.test_set_length+1]
+        self.labels_testing = digits.target[self.training_set_length+1:self.training_set_length+self.test_set_length+1]
+
+        # Flat versions of the images
+        self.images_training_flatted = np.reshape(self.images_training, [self.images_training.shape[0],
+                                                                         self.images_training.shape[1] *
+                                                                         self.images_training.shape[2]])
+
+        self.images_testing_flatted = np.reshape(self.images_testing, [self.images_testing.shape[0],
+                                                                       self.images_testing.shape[1] *
+                                                                       self.images_testing.shape[2]])
+
+    def load_DigitsTrainningData(self):
+        # Loads the MINST Digits Data. The images come flatted
+
+        mndata = MNIST()
+        self.images_training_flatted, self.labels_training = mndata.load_training()
+        self.images_testing_flatted, self.labels_testing = mndata.load_testing()
+
+
+
+        # Definition of numpy arrays
+        # self.images_training = np.array(self.images_training[:self.training_set_length])
+        self.images_training_flatted = np.array(self.images_training_flatted[:self.training_set_length])
+        self.labels_training = np.array(self.labels_training[:self.training_set_length])
+
+        # self.images_testing = np.array(self.images_testing[:self.test_set_length])
+        self.images_testing_flatted = np.array(self.images_testing_flatted[:self.test_set_length])
+        self.labels_testing = np.array(self.labels_testing[:self.test_set_length])
+
+        self.images_training_flatted = self.normalizeImages(self.images_training_flatted)
+        self.images_testing_flatted = self.normalizeImages(self.images_testing_flatted)
+
+
+        print(self.images_training_flatted.shape)
+        print(self.images_testing_flatted.shape)
+
+        # Definition of the flatten versions
+        height_width_image_set = round(sqrt(self.images_training_flatted.shape[1]))
+        length_training_set = self.images_training_flatted.shape[0]
+        length_testing_set = self.images_testing_flatted.shape[0]
+        print('Here')
+        print(height_width_image_set)
+        print(length_training_set)
+        print(length_testing_set )
+
+
+        self.images_training = np.reshape(self.images_training_flatted, [length_training_set,height_width_image_set,height_width_image_set])
+        self.images_testing  = np.reshape(self.images_testing_flatted,[length_testing_set, height_width_image_set, height_width_image_set])
+
+    def normalizeImages(self, image_NP_Array):
+        # Set the color image (black and white) to values between 0 and 255
+        for i in range(image_NP_Array.shape[0]):
+            image_NP_Array[i, :] = image_NP_Array[i, :]*(255.0/image_NP_Array[i, :].max())
+        return image_NP_Array
+
+    def classifierDef_SVM(self,training_set_length):
+        # Creates a classifier based on Support Vector Machine Algorithm
+
+        # self.classifier_svm = svm.SVC(gamma=0.001)
+        self.classifier_svm = svm.SVC(gamma=0.0000001)
+        self.classifier_svm.fit(self.images_training_flatted, self.labels_training)
+
+    def classify_SVM(self,image):
+        return self.classifier_svm.predict([image])[0]
+
+    def testRandomImage(self):
+        idx_random = random.randint(0,self.test_set_length)
+        image_test_flatted = self.images_testing_flatted[idx_random]
+        label_test = self.labels_testing[idx_random]
+        label_id = self.classify_SVM(image_test_flatted)
+        print('Label expected:  '+str(label_test) +', Label identified: '+str(label_id))
+        print('Max Value', image_test_flatted.max())
+        print(image_test_flatted)
+
+
+digitsClassifier = DigitsClassifier()
+digitsClassifier.testRandomImage()
